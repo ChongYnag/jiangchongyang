fa049b69386643ef1a4126370a313e6e9e84abd3
jiangchongyang <jiangchongyang@onething.net>
2019-10-28 20:23:02 +0800

    fixl

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,23 +1,31 @@
 const path = require('path');
 module.exports = {
-  title: 'Awesome',
-  description: 'Just playing around',
-  dest: path.resolve(__dirname, '../../docs'),
-  themeConfig: {
-    sidebar: [
-      {
-        title: 'Interview',
-        //  path: '/interview/',
-        collapsable: false,
-        children: [
-          '/interview/inherit',
-          '/interview/xss_csrf',
-          '/interview/event_loop'
+    title: '前端知识梳理',
+    description: 'Just playing around',
+    dest: path.resolve(__dirname, '../../docs'),
+    themeConfig: {
+        sidebar: [
+            {
+                title: 'js经典问题',
+                //  path: '/interview/',
+                collapsable: false,
+                children: [
+                    '/js/call_apply_bind',
+                ]
+            },
+            {
+                title: 'Interview',
+                //  path: '/interview/',
+                collapsable: false,
+                children: [
+                    '/interview/inherit',
+                    '/interview/xss_csrf',
+                    '/interview/event_loop'
+                ]
+            }
         ]
-      }
-    ]
-  },
-  markdown: {
-    lineNumbers: true
-  }
+    },
+    markdown: {
+        lineNumbers: true
+    }
 }
---
+++ src/js/call_apply_bind.md
@@ -0,0 +1,24 @@
+# call、apply、bind实现及详解
+
+### 在JS中，这三者都是用来改变函数的this对象的指向的，他们有什么样的区别呢。
+在说区别之前还是先总结一下三者的相似之处：
+- 都是用来改变函数的this对象的指向的。
+- 第一个参数都是this要指向的对象。
+- 都可以利用后续参数传参。
+
+## call
+> call 接收多个参数，第一个为函数上下文也就是this，后边参数为函数本身的参数。
+
+```js
+let obj ={
+    name: "一个"
+}
+function allName(firstName, lastName) {
+    console.log(this)
+    console.log(`我的全名是“${firstName}${this.name}${lastName}”`)
+}
+// 很明显此时allName函数是没有name属性的
+allName('我是', '前端') //我的全名是“我是前端”  this指向window
+allName.call(obj, '我是', '前端') //我的全名是“我是一个前端” this指向obj
+```
+![输出结果](/call.png)
